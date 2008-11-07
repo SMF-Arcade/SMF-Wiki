d06518735a143fb67c2d801ddfd81f398dfd9e31
niko <niko@8c78d067-d79e-45ab-b2a9-ef1b98dd6782>
2008-11-07 17:42:46 +0000

    testing

--- Package/build.php
+++ Package/build.php
@@ -1,4 +1,24 @@
 <?php
+/**********************************************************************************
+* build.php                                                                       *
+***********************************************************************************
+* SMF Wiki                                                                        *
+* =============================================================================== *
+* Software Version:           SMF Wiki 0.1                                        *
+* Software by:                Niko Pahajoki (http://www.madjoki.com)              *
+* Copyright 2008 by:          Niko Pahajoki (http://www.madjoki.com)              *
+* Support, News, Updates at:  http://www.smfarcade.info                           *
+***********************************************************************************
+* This program is free software; you may redistribute it and/or modify it under   *
+* the terms of the provided license as published by Simple Machines LLC.          *
+*                                                                                 *
+* This program is distributed in the hope that it is and will be useful, but      *
+* WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY    *
+* or FITNESS FOR A PARTICULAR PURPOSE.                                            *
+*                                                                                 *
+* See the "license.txt" file for details of the Simple Machines license.          *
+* The latest version can always be found at http://www.simplemachines.org.        *
+**********************************************************************************/
 
 // Build info
 $build_info = array(
