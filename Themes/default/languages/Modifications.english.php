fdbc3e7fb93cb403148343e315eb1aacbe292e8d
niko <niko@8c78d067-d79e-45ab-b2a9-ef1b98dd6782>
2008-11-08 10:01:39 +0000

    ! added permissions to ManagePermissions

--- Themes/default/languages/Modifications.english.php
+++ Themes/default/languages/Modifications.english.php
@@ -9,4 +9,18 @@ $txt['admin_wiki'] = 'Wiki';
 $txt['admin_wiki_main'] = 'Main';
 $txt['admin_wiki_settings'] = 'Settings';
 
+// Permission Names
+$txt['permissiongroup_wiki'] = 'Wiki';
+$txt['permissionname_wiki_access'] = 'Access Wiki';
+$txt['permissionname_wiki_edit'] = 'Edit Pages In Wiki';
+$txt['permissionname_wiki_admin'] = 'Administrate Wiki';
+
+// Simple permission gropus
+$txt['permissiongroup_simple_wiki'] = 'Use Wiki';
+
+// Errors
+$txt['cannot_wiki_access'] = 'You are not allowed to access Wiki.';
+$txt['cannot_wiki_edit'] = 'You are not allowed to edit pages.';
+$txt['cannot_wiki_admin'] = 'You are not allowed to administrate Wiki.';
+
 ?>
